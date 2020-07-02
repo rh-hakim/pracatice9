3814c3a75b860bca174ad6d0a16b41da88d4f658
unknown <rh.hakim2020@gmail.com>
2020-07-02 14:21:24 -0700

    first commit to it

---
+++ src/main/java/POMPractice/POMPractice/App.java
@@ -0,0 +1,13 @@
+package POMPractice.POMPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
